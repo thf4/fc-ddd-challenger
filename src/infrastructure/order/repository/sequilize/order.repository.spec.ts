064d15cfa143adec0b1ed0cdb82d968595fced1c
Thales Farias <ths.farias64@hotmail.com>
2023-11-11 11:11:46 -0300

    chore: add order repository

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -81,4 +81,145 @@ describe("Order repository test", () => {
       ],
     });
   });
+  it("should update a order total", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const otherProduct = new Product("124", "Product 2", 15);
+    await productRepository.create(otherProduct);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+    const orderItem2 = new OrderItem(
+      "2",
+      otherProduct.name,
+      otherProduct.price,
+      otherProduct.id,
+      1
+    );
+
+    const order = new Order("123", "123", [orderItem, orderItem2]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+
+    expect(orderModel.total).toBe(order.total());
+    expect(orderModel.items.length).toBe(2);
+
+    const newOrderItem = new OrderItem(
+      "8",
+      product.name,
+      product.price,
+      product.id,
+      1
+    );
+
+    const newOrder = new Order("123", "123", [newOrderItem]);
+    await orderRepository.update(newOrder);
+
+    const orderModel2 = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+
+    expect(orderModel2.total).toBe(newOrder.total());
+    expect(orderModel2.items.length).toBe(2);
+  });
+
+  it("should find a order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderRepository = new OrderRepository();
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+    const order = new Order("123", "123", [orderItem]);
+    await orderRepository.create(order);
+
+    const orderResult = await orderRepository.find(order.id);
+
+    expect(order).toStrictEqual(orderResult);
+  });
+  
+  it("should find all orders", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product1 = new Product("23", "Shoes", 259.99);
+    await productRepository.create(product1);
+
+    const product2 = new Product("24", "Shirt", 89.99);
+    await productRepository.create(product2);
+
+    const product3 = new Product("25", "Pack of socks", 19.99);
+    await productRepository.create(product3);
+
+    const orderItem1 = new OrderItem(
+      "247",
+      "Order Item 1",
+      product1.price,
+      product1.id,
+      1
+    );
+    const orderItem2 = new OrderItem(
+      "248",
+      "Order Item 2",
+      product2.price,
+      product2.id,
+      2
+    );
+    const orderItem3 = new OrderItem(
+      "249",
+      "Order Item 3",
+      product3.price,
+      product3.id,
+      1
+    );
+
+    const orderRepository = new OrderRepository();
+    const order1 = new Order("329", customer.id, [orderItem1, orderItem2]);
+    await orderRepository.create(order1);
+
+    const order2 = new Order("330", customer.id, [orderItem3]);
+    await orderRepository.create(order2);
+
+    const orders = await orderRepository.findAll();
+
+    expect(orders.length).toBe(2);
+    expect(orders).toContainEqual(order1);
+    expect(orders).toContainEqual(order2);
+  });
 });
--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from '../../../../domain/checkout/entity/order_item';
+import OrderRepositoryInterface from '../../../../domain/checkout/repository/order-repository.interface';
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -21,5 +23,65 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    await OrderModel.update(
+      {
+        total: entity.total(),
+        customer_id: entity.customerId,
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
+  }
+
+  async find(id: string): Promise<Order> {
+    const order = await OrderModel.findOne({
+      where: { id },
+      include: ["items"],
+    });
+    const list: OrderItem[] = [];
+    order.items.map((item) => {
+      let i = new OrderItem(
+        item.id,
+        item.name,
+        item.price,
+        item.product_id,
+        item.quantity
+      );
+      list.push(i);
+    });
+    return new Order(order.id, order.customer_id, list);
+  }
+
+  async findAll(): Promise<Order[]> {
+    const orderModels = await OrderModel.findAll({ include: [OrderItemModel] });
+    const orders = orderModels.map((orderModel) => {
+      const list: OrderItem[] = [];
+      orderModel.items.map((item) => {
+        let i = new OrderItem(
+          item.id,
+          item.name,
+          item.price,
+          item.product_id,
+          item.quantity
+        );
+        list.push(i);
+      });
+      return new Order(orderModel.id, orderModel.customer_id, list);
+    });
+
+    return orders;
   }
 }
